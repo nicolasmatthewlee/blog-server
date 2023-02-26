f59c0e8b4dacea6e35073f3adac312256a1c04e7
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-02-25 20:28:37 -0800

    Moved /Users to its own router and renamed routes as follows: POST /signup => POST /users, GET /user => GET /users, POST /signin => POST /users/login, POST /signout => POST /users/logout

--- app.ts
+++ app.ts
@@ -1,20 +1,17 @@
-import express, { NextFunction, Request, Response, text } from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
 mongoose.set("strictQuery", false);
-import { Article, articleI } from "./models/article";
-import { User, userI } from "./models/user";
 import cors from "cors";
-import { body, validationResult } from "express-validator";
 const session = require("express-session");
 import MongoStore from "connect-mongo";
 const passport = require("passport");
 require("./passport.ts");
-const bcrypt = require("bcryptjs");
-import { Notification, notificationI } from "./models/notification";
+
 const ArticlesRouter = require("./routes/articles");
+const UsersRouter = require("./routes/users");
 
 declare module "express" {
   export interface Request {
@@ -34,52 +31,7 @@ mongoose.connection.on("error", (err: Error) => {
 
 const app = express();
 app.use(express.json({ limit: "50mb" }));
-
 app.use(cors({ credentials: true, origin: "http://127.0.0.1:3000" }));
-
-app.post("/signup", [
-  body("username")
-    .trim()
-    .escape()
-    .isLength({ min: 1 })
-    .withMessage("Username must be specified")
-    .isLength({ max: 50 })
-    .withMessage("username must not exceed 50 characters")
-    .custom((value) => {
-      return User.findOne({ username: value }).then((user) => {
-        if (user) return Promise.reject("Username already in use");
-      });
-    }),
-  body("password")
-    .trim()
-    .escape()
-    .isLength({ min: 1 })
-    .withMessage("Password must be specified")
-    .isLength({ max: 50 })
-    .withMessage("password must not exceed 50 characters"),
-  (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) return res.json(errors);
-
-    bcrypt.hash(req.body.password, 10, (err: any, hashedPassword: string) => {
-      if (err) return next(err);
-
-      const user = new User({
-        username: req.body.username,
-        password: hashedPassword,
-        saved: [],
-        liked: [],
-        notifications: [],
-      });
-
-      user.save((err, result) => {
-        if (err) return res.json(err);
-        return res.json({ saved: true });
-      });
-    });
-  },
-]);
-
 app.use(
   session({
     secret: process.env.secret,
@@ -88,168 +40,10 @@ app.use(
     store: MongoStore.create({ mongoUrl: process.env.mongo }),
   })
 );
-
 app.use(passport.authenticate("session"));
 
 app.use("/articles", ArticlesRouter);
-
-app.post("/signin", [
-  body("username")
-    .trim()
-    .escape()
-    .isLength({ min: 1 })
-    .withMessage("Username must be specified")
-    .isLength({ max: 50 })
-    .withMessage("username must not exceed 50 characters"),
-  body("password")
-    .trim()
-    .escape()
-    .isLength({ min: 1 })
-    .withMessage("Password must be specified")
-    .isLength({ max: 50 })
-    .withMessage("password must not exceed 50 characters"),
-  (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) return res.json(errors);
-    next();
-  },
-  (req: Request, res: Response, next: NextFunction) =>
-    passport.authenticate("local", (err: string, user: userI) => {
-      if (err) return next(err);
-      if (!user)
-        return res.json({
-          errors: [{ message: "Username or password is incorrect" }],
-        });
-
-      if (req.login)
-        req.login(user, (err: string) => {
-          if (err) return next(err);
-          return res.json({ success: true });
-        });
-    })(req, res, next),
-]);
-
-app.get("/user", (req: Request, res: Response, next: NextFunction) => {
-  return req.user
-    ? res.json({
-        _id: req.user._id,
-        username: req.user.username,
-        saved: req.user.saved,
-        liked: req.user.liked,
-      })
-    : res.json({ errors: [{ message: "Unauthorized" }] });
-});
-
-app.post("/signout", (req: Request, res: Response, next: NextFunction) => {
-  if (req.logout)
-    return req.logout((err: any) => {
-      if (err) return next(err);
-      res.json({ success: true });
-    });
-  next();
-});
-
-app.delete(
-  "/users/:userId",
-  (req: Request, res: Response, next: NextFunction) => {
-    User.findByIdAndDelete(req.params.userId, (err: any, result: any) => {
-      if (err) return next(err);
-      else return res.json({ success: true });
-    });
-  }
-);
-
-app.post(
-  "/users/:userId/saved",
-  (req: Request, res: Response, next: NextFunction) => {
-    if (Number(req.body.toStatus) === 1) {
-      User.findByIdAndUpdate(
-        req.params.userId,
-        { $addToSet: { saved: req.body.articleId } },
-        (err: any, result: any) => {
-          if (err) return next(err);
-          else return res.json({ result: 1 });
-        }
-      );
-    } else {
-      User.findByIdAndUpdate(
-        req.params.userId,
-        { $pull: { saved: req.body.articleId } },
-        (err: any, result: any) => {
-          if (err) return next(err);
-          else return res.json({ result: 0 });
-        }
-      );
-    }
-  }
-);
-
-app.post(
-  "/users/:userId/liked",
-  (req: Request, res: Response, next: NextFunction) => {
-    if (Number(req.body.toStatus) === 1) {
-      User.findByIdAndUpdate(
-        req.params.userId,
-        { $addToSet: { liked: req.body.articleId } },
-        (err: any, result: any) => {
-          if (err) return next(err);
-          else return res.json({ result: 1 });
-        }
-      );
-    } else {
-      User.findByIdAndUpdate(
-        req.params.userId,
-        { $pull: { liked: req.body.articleId } },
-        (err: any, result: any) => {
-          if (err) return next(err);
-          else return res.json({ result: 0 });
-        }
-      );
-    }
-  }
-);
-
-app.post(
-  "/users/:authorId/notifications",
-  (req: Request, res: Response, next: NextFunction) => {
-    const notification = new Notification({
-      user: req.body.userId,
-      event: req.body.event,
-      time: new Date(),
-      resource: req.body.resource,
-    });
-
-    notification.save((err, createdNotification) => {
-      if (err) return next(err);
-
-      User.findByIdAndUpdate(
-        req.params.authorId,
-        { $addToSet: { notifications: createdNotification._id } },
-        (err: any, result: any) => {
-          if (err) return next(err);
-          else return res.json({ success: true });
-        }
-      );
-    });
-  }
-);
-
-app.get(
-  "/users/:userId/notifications",
-  (req: Request, res: Response, next: NextFunction) => {
-    Notification.find({
-      _id: {
-        $in: req.user.notifications,
-      },
-    })
-      .populate("resource", "title")
-      .populate("user", "username")
-      .exec((err: any, notifications) => {
-        if (err) return next(err);
-        return res.json(notifications);
-      });
-  }
-);
+app.use("/users", UsersRouter);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err) {
---
+++ routes/users.ts
@@ -0,0 +1,212 @@
+import { NextFunction, Request, Response, Router } from "express";
+import { body, validationResult } from "express-validator";
+import { User } from "../models/user";
+import { Notification } from "../models/notification";
+import { userI } from "../models/user";
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
+const router = Router();
+
+router.get("/", [
+  (req: Request, res: Response, next: NextFunction) => {
+    if (req.user)
+      return req.user
+        ? res.json({
+            _id: req.user._id,
+            username: req.user.username,
+            saved: req.user.saved,
+            liked: req.user.liked,
+          })
+        : res.json({ errors: [{ message: "Unauthorized" }] });
+    else next();
+  },
+]);
+
+router.post("/login", [
+  body("username")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("Username must be specified")
+    .isLength({ max: 50 })
+    .withMessage("username must not exceed 50 characters"),
+  body("password")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("Password must be specified")
+    .isLength({ max: 50 })
+    .withMessage("password must not exceed 50 characters"),
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.json(errors);
+    next();
+  },
+  (req: Request, res: Response, next: NextFunction) =>
+    passport.authenticate("local", (err: string, user: userI) => {
+      if (err) return next(err);
+      if (!user)
+        return res.json({
+          errors: [{ message: "Username or password is incorrect" }],
+        });
+
+      if (req.login)
+        req.login(user, (err: string) => {
+          if (err) return next(err);
+          return res.json({ success: true });
+        });
+    })(req, res, next),
+]);
+
+router.post("/logout", (req: Request, res: Response, next: NextFunction) => {
+  if (req.logout)
+    return req.logout((err: any) => {
+      if (err) return next(err);
+      res.json({ success: true });
+    });
+  next();
+});
+
+router.post("/", [
+  body("username")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("Username must be specified")
+    .isLength({ max: 50 })
+    .withMessage("username must not exceed 50 characters")
+    .custom((value) => {
+      return User.findOne({ username: value }).then((user) => {
+        if (user) return Promise.reject("Username already in use");
+      });
+    }),
+  body("password")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("Password must be specified")
+    .isLength({ max: 50 })
+    .withMessage("password must not exceed 50 characters"),
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.json(errors);
+
+    bcrypt.hash(req.body.password, 10, (err: any, hashedPassword: string) => {
+      if (err) return next(err);
+
+      const user = new User({
+        username: req.body.username,
+        password: hashedPassword,
+        saved: [],
+        liked: [],
+        notifications: [],
+      });
+
+      user.save((err, result) => {
+        if (err) return res.json(err);
+        return res.json({ saved: true });
+      });
+    });
+  },
+]);
+
+router.delete("/:userId", (req: Request, res: Response, next: NextFunction) => {
+  User.findByIdAndDelete(req.params.userId, (err: any, result: any) => {
+    if (err) return next(err);
+    else return res.json({ success: true });
+  });
+});
+
+router.post(
+  "/:userId/saved",
+  (req: Request, res: Response, next: NextFunction) => {
+    if (Number(req.body.toStatus) === 1) {
+      User.findByIdAndUpdate(
+        req.params.userId,
+        { $addToSet: { saved: req.body.articleId } },
+        (err: any, result: any) => {
+          if (err) return next(err);
+          else return res.json({ result: 1 });
+        }
+      );
+    } else {
+      User.findByIdAndUpdate(
+        req.params.userId,
+        { $pull: { saved: req.body.articleId } },
+        (err: any, result: any) => {
+          if (err) return next(err);
+          else return res.json({ result: 0 });
+        }
+      );
+    }
+  }
+);
+
+router.post(
+  "/:userId/liked",
+  (req: Request, res: Response, next: NextFunction) => {
+    if (Number(req.body.toStatus) === 1) {
+      User.findByIdAndUpdate(
+        req.params.userId,
+        { $addToSet: { liked: req.body.articleId } },
+        (err: any, result: any) => {
+          if (err) return next(err);
+          else return res.json({ result: 1 });
+        }
+      );
+    } else {
+      User.findByIdAndUpdate(
+        req.params.userId,
+        { $pull: { liked: req.body.articleId } },
+        (err: any, result: any) => {
+          if (err) return next(err);
+          else return res.json({ result: 0 });
+        }
+      );
+    }
+  }
+);
+
+router.post(
+  "/:authorId/notifications",
+  (req: Request, res: Response, next: NextFunction) => {
+    const notification = new Notification({
+      user: req.body.userId,
+      event: req.body.event,
+      time: new Date(),
+      resource: req.body.resource,
+    });
+
+    notification.save((err, createdNotification) => {
+      if (err) return next(err);
+
+      User.findByIdAndUpdate(
+        req.params.authorId,
+        { $addToSet: { notifications: createdNotification._id } },
+        (err: any, result: any) => {
+          if (err) return next(err);
+          else return res.json({ success: true });
+        }
+      );
+    });
+  }
+);
+
+router.get(
+  "/:userId/notifications",
+  (req: Request, res: Response, next: NextFunction) => {
+    Notification.find({
+      _id: {
+        $in: req.user.notifications,
+      },
+    })
+      .populate("resource", "title")
+      .populate("user", "username")
+      .exec((err: any, notifications) => {
+        if (err) return next(err);
+        return res.json(notifications);
+      });
+  }
+);
+
+module.exports = router;
